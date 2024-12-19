f21bb049c2be3441da579cd35c81819298049fac
Danyal Rana <ranadanyalarshad@gmail.com>
2024-12-19 23:42:22 +0500

    Enhance user model with password hashing, JWT token generation, and additional fields

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,54 +1,85 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-    {
-        fullName: {
-            type: String,
-            required: true,
-        },
-        username:{
-            type: String,
-            required: true,
-            unique: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            match: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        role: {
-            type: String,
-            enum: ['user', 'admin'], // Only these two roles
-            default: 'user',
-        },
-        secretKey: {
-            type: String,
-            default: null,
-        },
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
     },
-    {
-        timestamps: true,
-    }
-);
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true, // used for searching
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/\S+@\S+\.\S+/, 'is invalid'],
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    refreshToken: {
+        type: String,
+    },
+    secretKey: {
+        type: String,
+        default: null,
+    },
+    avatar: {
+        type: String, // cloudinary url
+    },
 
-// Hash password before saving
-userSchema.pre('save', async function (next) {
-    if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
+}, { timestamps: true });
+
+// arrow function does not have its own this, but in this case we need the this reference, that's why classical arrow function is not recommended here
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    };
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 });
 
-// Compare input password with stored password
-userSchema.methods.comparePassword = async function (inputPassword) {
-    return await bcrypt.compare(inputPassword, this.password); // Compare password for login
+
+// .methods already has many functions, also we can add our own custom function to it
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+// .sign genertes the token, it takes 3 arguments, 1st is payload(data){}, 2nd is secret key, 3rd is expiry date{}
+// .sign has basic encrption,( it's not the best way to encrypt, but it's good for small projects)
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        // here we're giving payload(data) to this token
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName,
+    }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    });
+};
+
+// Refresh token has less info because it keeps on refreshing (it's not going to be sent to the client)
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        // here we're giving payload(data) to this token
+        _id: this._id,
+    },
+        process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    });
 };
 
 // Method to apply for admin role (using secret key)
@@ -64,5 +95,4 @@ userSchema.methods.applyForAdminRole = function (inputSecretKey) {
     }
 };
 
-const User = mongoose.model('User', userSchema);
-export default User;
+export const User = mongoose.model("User", userSchema);
