a69558640ffebea1bb74b2d9a05f484ece0d98d2
Danyal Rana <ranadanyalarshad@gmail.com>
2024-12-21 22:45:56 +0500

    Update certificate template routes for improved clarity and consistency

--- server/src/routes/certificateTemplate.routes.js
+++ server/src/routes/certificateTemplate.routes.js
@@ -9,12 +9,12 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 // Create a new certificate template (protected)
-router.route("/templates").post(verifyJWT, createTemplate);
+router.route("/create-template").post(verifyJWT, createTemplate);
 
 // Fetch all templates for the logged-in user (protected)
-router.route("/templates").get(verifyJWT, getUserTemplates);
+router.route("/get-templates").get(verifyJWT, getUserTemplates);
 
 // Delete a specific template by ID (protected)
-router.route("/templates/:id").delete(verifyJWT, deleteTemplate);
+router.route("/delete-template/:id").delete(verifyJWT, deleteTemplate);
 
 export default router;
