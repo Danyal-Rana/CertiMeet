48a685d23d410fcce4f618dfc5644b3981d12edf
Danyal Rana <ranadanyalarshad@gmail.com>
2024-12-22 01:55:34 +0500

    Add csv-parser and xlsx dependencies; update file model to include Cloudinary fields

--- server/src/models/file.model.js
+++ server/src/models/file.model.js
@@ -4,31 +4,19 @@ const fileSchema = new mongoose.Schema({
     fileName: { 
         type: String, 
         required: true 
-
-    },
-    filePath: { 
-        type: String, 
-        required: true 
-
     },
     owner: { 
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'User', 
         required: true 
-
-    }, // Link to user who uploaded
+    },
     type: { 
         type: String, 
         enum: ['csv', 'xlsx'], 
         required: true 
-
-    }, // File type: csv or excel
-    status: { 
-        type: String, 
-        enum: ['pending', 'processed'], 
-        default: 'pending' 
-
-    }, // Track if the file has been processed
+    },
+    public_id: { type: String },  // Cloudinary public_id
+    secure_url: { type: String }, // Cloudinary secure_url
 }, {
     timestamps: true
 });
