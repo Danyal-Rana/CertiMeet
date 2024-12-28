84110b974c70b58793ec6484cf7b6e912e7278b9
Danyal Rana <ranadanyalarshad@gmail.com>
2024-12-29 00:06:48 +0500

    Refactor Footer component for improved layout and update description for clarity

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,12 @@
 const Footer = () => {
     return (
         <footer className="bg-gray-800">
-            <div className="max-w-8xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+            <div className="max-w-6xl mx-auto py-4 px-4 sm:px-6 lg:py-6 lg:px-8">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     <div>
                         <img className="h-8" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2B9LswCEh3jElEIiDsU4C60RTU3Chlv-h2A&s" alt="CertiMeet" />
                         <p className="mt-4 text-gray-300 text-base">
-                            Streamline your certificate generation and meeting management with CertiMeet.
+                        CertiMeet simplifies your workflow by seamlessly combining certificate generation and meeting scheduling into a single, user-friendly platform. Whether you’re organizing events, managing teams, or handling certifications, CertiMeet ensures efficiency and professionalism at every step.
                         </p>
                         <div className="mt-4 flex space-x-6">
                             <a href="#" className="text-gray-400 hover:text-gray-300">
@@ -45,7 +45,7 @@ const Footer = () => {
 
                     </div>
                 </div>
-                <div className="mt-8 border-t border-gray-700 pt-8">
+                <div className="mt-8 border-t border-gray-700 pt-2">
                     <p className="text-base text-gray-400 text-center">
                         © 2024 CertiMeet. All rights reserved. Powered by{' '}
                         <a
