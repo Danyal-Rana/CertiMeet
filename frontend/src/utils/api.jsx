92ac35ce276dd91ec08e8ec194e4353a92a783f7
Danyal Rana <ranadanyalarshad@gmail.com>
2024-12-31 02:31:22 +0500

    Migrate API utility from api.js to api.jsx; enhance request interceptor to include authorization token from cookies

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,36 +1,3 @@
-// import axios from 'axios';
-
-// const api = axios.create({
-//     baseURL: 'http://localhost:8000/api/v1',
-//     withCredentials: true,
-// });
-
-// // Add request interceptor
-// api.interceptors.request.use(
-//     (config) => {
-//         console.log('API Request:', config.method.toUpperCase(), config.url, config.data);
-//         return config;
-//     },
-//     (error) => {
-//         console.error('API Request Error:', error);
-//         return Promise.reject(error);
-//     }
-// );
-
-// // Add response interceptor
-// api.interceptors.response.use(
-//     (response) => {
-//         console.log('API Response:', response.status, response.data);
-//         return response;
-//     },
-//     (error) => {
-//         console.error('API Response Error:', error.response?.status, error.response?.data || error.message);
-//         return Promise.reject(error);
-//     }
-// );
-
-// export default api;
-
 import axios from 'axios';
 
 const api = axios.create({
