2b261e0557fd957e028130e3286ee8f8ed719242
Danyal Rana <ranadanyalarshad@gmail.com>
2025-02-14 17:06:06 +0500

    Fix session secret usage and enforce secure cookies in session middleware

--- server/src/app.js
+++ server/src/app.js
@@ -25,10 +25,10 @@ app.use(cookieParser()); // to set and access the cookies of the client in the b
 
 // Session middleware setup
 app.use(session({
-    secret: 'process.env.SESSION_SECRET', // Use a strong secret key
+    secret: process.env.SESSION_SECRET, // Use a strong secret key
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } // Set to true if you're using HTTPS
+    cookie: { secure: true } // Set to true if you're using HTTPS
 }));
 
 // Routes
